4a42f3dac3fc95527f496bd1477376f99b8cbe58
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2025-04-18 16:52:38 +0530

    nextConfig ✨

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // webpack(config, { isServer, dev }) {
-  //   config.experiments = {
-  //     asyncWebAssembly: true,
-  //     layers: true,
-  //   };
+  webpack(config, { isServer, dev }) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
 
-  //   return config;
-  // },
+    return config;
+  },
 };
 
 export default nextConfig;
