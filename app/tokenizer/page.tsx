82844875b79472429f3e4ff8a906b53ecdf631a5
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2025-04-22 18:13:08 +0530

    changes 🛠️

--- app/tokenizer/page.tsx
+++ app/tokenizer/page.tsx
@@ -6,23 +6,21 @@ import OutputBox from "@/components/OutputBox";
 
 export default function Home() {
   const [tokens, setTokens] = useState<Uint32Array>(new Uint32Array());
- 
-  const handleInputSubmit = async (model:TiktokenModel,newInput: string) => {
- 
+
+  const handleInputSubmit = async (model: TiktokenModel, newInput: string) => {
     const res = await fetch("/api/tokenize", {
       method: "POST",
       body: JSON.stringify({ text: newInput, model }),
       headers: { "Content-Type": "application/json" },
     });
     const data = await res.json();
-    setTokens(data.tokens);
+    setTokens(new Uint32Array(data.tokens));
   };
-  
+
   return (
     <div>
       <div className="text-center">
         <h1 className="text-2xl font-bold">Tokenizer</h1>
-       
       </div>
 
       <div className="flex sm:flex-row flex-col items-center justify-center gap-5 p-5 ">
